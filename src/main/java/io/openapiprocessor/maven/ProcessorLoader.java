bfba483bcd51e2b87447f50fac643096e58384c9
Martin Hauner <martin.hauner@gmx.net>
2021-02-27 18:51:33 +0100

    simpler

--- src/main/java/io/openapiprocessor/maven/ProcessorLoader.java
+++ src/main/java/io/openapiprocessor/maven/ProcessorLoader.java
@@ -1,17 +1,6 @@
 /*
- * Copyright 2019-2020 the original authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2019 https://github.com/openapi-processor/openapi-processor-maven
+ * PDX-License-Identifier: Apache-2.0
  */
 
 package io.openapiprocessor.maven;
@@ -19,9 +8,7 @@ package io.openapiprocessor.maven;
 import java.util.*;
 
 /**
- * Find the processors using the ServiceLoader.
- *
- * @author Martin Hauner
+ * find the processors using the ServiceLoader.
  */
 class ProcessorLoader {
 
